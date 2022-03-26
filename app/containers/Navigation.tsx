2b83413d3ca95a648eb025bf7967038b04953f35
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-03-26 12:31:48 +0300

    Use isLoggedIn from session context in navigation

--- app/containers/Navigation.tsx
+++ app/containers/Navigation.tsx
@@ -4,15 +4,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigation from './auth/AuthNavigation';
 import MainNavigation from './main/MainNavigation';
+import { useSessionContext } from '../context/SessionContext';
 
 const Stack = createNativeStackNavigator();
 
 const Navigation: FC = () => {
-  const isLoggedIn = true;
+  const { sessionData } = useSessionContext();
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {isLoggedIn ? (
+        {sessionData?.isLoggedIn ? (
           <Stack.Screen name="MainNavigator" component={MainNavigation} />
         ) : (
           <Stack.Screen name="AuthNavigator" component={AuthNavigation} />
