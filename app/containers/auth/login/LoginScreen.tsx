8b8e66d411afe5b13fcd8ee19a13222d2cfb67d4
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-03-26 22:50:50 +0300

    Edit login screen style

--- app/containers/auth/login/LoginScreen.tsx
+++ app/containers/auth/login/LoginScreen.tsx
@@ -1,11 +1,83 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
+import Button from '../../../components/button/Button';
+import { useTheme } from '../../../theme/theme';
+
+import styles from './styles/LoginScreen.style';
+
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const LoginScreen = () => {
+  const theme = useTheme();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <KeyboardAwareScrollView
+      style={[styles.scrollView, { backgroundColor: theme.background }]}
+      enableOnAndroid={true}
+      bounces={false}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={theme.textStyles.loginTitle}>InstaClone</Text>
+        </View>
+        <View style={styles.body}>
+          <View style={styles.formItem}>
+            <View
+              style={[
+                styles.inputView,
+                {
+                  backgroundColor: theme.secondary,
+                  borderColor: theme.tertiary,
+                },
+              ]}>
+              <TextInput
+                style={[styles.input, theme.textStyles.largeInput]}
+                placeholder="Phone, username or e-mail"
+                autoCapitalize="none"
+                value={username}
+                onChangeText={setUsername}
+              />
+            </View>
+          </View>
+          <View style={styles.formItem}>
+            <View
+              style={[
+                styles.inputView,
+                {
+                  backgroundColor: theme.secondary,
+                  borderColor: theme.tertiary,
+                },
+              ]}>
+              <TextInput
+                style={[styles.input, theme.textStyles.largeInput]}
+                placeholder="Enter password"
+                secureTextEntry={true}
+                autoComplete="password"
+                value={password}
+                onChangeText={setPassword}
+              />
+            </View>
+          </View>
+          <View
+            style={[
+              styles.footer,
+              { backgroundColor: theme.primary },
+              (username.length === 0 || password.length === 0) && {
+                opacity: 0.4,
+              },
+            ]}>
+            <Button
+              text={'Login'}
+              onPress={() => console.log('Click')}
+              disabled={username.length === 0 || password.length === 0}
+            />
+          </View>
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
   );
 };
 
