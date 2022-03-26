5fbd3f32842e255a2651a3e90483a91280dc3456
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-03-26 14:55:17 +0300

    Create loading screen

--- app/containers/Navigation.tsx
+++ app/containers/Navigation.tsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigation from './auth/AuthNavigation';
 import MainNavigation from './main/MainNavigation';
 import { useSessionContext } from '../context/SessionContext';
+import LoadingScreen from './loading/LoadingScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,9 @@ const Navigation: FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {sessionData?.isLoggedIn ? (
+        {sessionData.isLoading && !sessionData.isLoggedIn ? (
+          <Stack.Screen name="Loading" component={LoadingScreen} />
+        ) : sessionData.isLoggedIn ? (
           <Stack.Screen name="MainNavigator" component={MainNavigation} />
         ) : (
           <Stack.Screen name="AuthNavigator" component={AuthNavigation} />
