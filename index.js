74f9998bd86decdac33beb1e41b92526d86ed298
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-03-26 11:46:55 +0300

    Delete app.tsx and edit index file by base navigation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
+import React, { FC } from 'react';
+import { AppRegistry, SafeAreaView } from 'react-native';
+import { name as appName } from './app.json';
+import Navigation from './app/containers/Navigation';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const Root: FC = () => (
+  <SafeAreaView style={{ flex: 1, backgroundColor: '#ffffff' }}>
+    <Navigation />
+  </SafeAreaView>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Root);
