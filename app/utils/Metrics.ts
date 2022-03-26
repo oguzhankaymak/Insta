01b8b7903d4fdb760ff07386e938359c4b80d82d
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-03-26 15:18:00 +0300

    Create metrics file for device metrics

---
+++ app/utils/Metrics.ts
@@ -0,0 +1,14 @@
+import { Dimensions } from 'react-native';
+
+export const deviceWidth = Dimensions.get('window').width;
+export const deviceHeight = Dimensions.get('window').height;
+
+const isTabletCheck = () => {
+  if (deviceHeight / deviceWidth > 1.6) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export const isTablet = isTabletCheck();
