9b5fca86d075a9231d7460b82f232109c0db551a
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2022-03-28 20:00:57 +0300

    Use post component and getPosts request in home screen

--- app/containers/main/home/HomeScreen.tsx
+++ app/containers/main/home/HomeScreen.tsx
@@ -1,11 +1,32 @@
-import React, { FC } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList } from 'react-native';
+import { getPosts } from '../../../actions/HomeAction';
+import { SUCCESS } from '../../../actions/Status';
+import Post, { IPost } from '../../../components/post/Post';
+import { useTheme } from '../../../theme/theme';
 
-const HomeScreen: FC = () => {
+import styles from './styles/HomeScreen.style';
+
+const HomeScreen = () => {
+  const theme = useTheme();
+  const [posts, setPosts] = useState<IPost[]>();
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await getPosts();
+    if (response.result === SUCCESS) {
+      setPosts(response.data);
+    }
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <FlatList
+      style={[styles.container, { backgroundColor: theme.background }]}
+      data={posts}
+      renderItem={({ item }) => <Post postData={item} />}
+    />
   );
 };
 
